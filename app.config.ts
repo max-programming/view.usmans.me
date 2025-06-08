0dac219fc559c9b1fc8096e602023aa371c8a9c1
Usman S. <ui.sabuwala@gmail.com>
2025-06-08 09:12:14 +0000

    Use netlify for deployment

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "@tanstack/react-start/config";
-import { cloudflare } from "unenv";
 import tsConfigPaths from "vite-tsconfig-paths";
-import nitroCloudflareBindings from "nitro-cloudflare-dev";
 
 export default defineConfig({
   server: {
-    preset: "cloudflare-module",
-    unenv: cloudflare,
-    modules: [nitroCloudflareBindings],
+    preset: "netlify",
   },
   vite: {
     plugins: [
